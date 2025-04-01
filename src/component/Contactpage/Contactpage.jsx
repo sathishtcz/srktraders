5f52f096bb9993702eb71e542a8ba469a0b7f71c
sathishtcz <msathish.tcz@gmail.com>
2025-04-01 12:55:22 +0530

    favicon

--- src/component/Contactpage/Contactpage.jsx
+++ src/component/Contactpage/Contactpage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Contact from "../../shared/component/Contact/Contact";
 
 export default function Contactpage() {
+    
     return (
         <div>
             <Contact/>
